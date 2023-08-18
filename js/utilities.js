d8636e64b4a1b9e0c82084fd0c460677e6c1c7b4
Morshed-17 <morshedmto@gmail.com>
2023-08-18 22:08:24 +0600

    active button added

--- js/utilities.js
+++ js/utilities.js
@@ -1,2 +1,18 @@
 // bold btn
 const inputField = document.getElementById('input-field')
+
+// active btns
+
+const buttons = document.querySelectorAll('button')
+
+buttons.forEach(button => {
+    button.addEventListener('click', function(){      
+        if (this.classList.contains('active')) {
+            this.classList.remove('active');
+        } else {
+            this.classList.add('active');
+        }
+        
+    })
+  
+})
